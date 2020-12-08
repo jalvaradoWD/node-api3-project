8bc0e6c2852966b0e14a58cc3b3ee58bbff9a61a
jalvaradoWD <jalvarado1337@lavabit.com>
2020-12-08 17:26:12 -0600

    Added the router for both the user and posts tables in the database.

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // code away!
 const { server, PORT } = require("./server.js");
+const userRouter = require("./users/userRouter");
+const postsRouter = require("./posts/postRouter");
+
+server.use("/api/users", userRouter);
+server.use("/api/posts", postsRouter);
 
 server.listen(PORT, () => console.log(`Server is on http://localhost:${PORT}`));
